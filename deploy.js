69a39debab68fa922dce139aadbd55f43813afe5
Shubham Gandhi <paskgandhi@gmail.com>
2021-08-31 23:36:11 +0530

    writter tests for smart contract

--- deploy.js
+++ deploy.js
@@ -3,22 +3,21 @@ const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-  "juice bicycle seek common shield hello below angry source share exact mobile",
-  // remember to change this to your own phrase!
-  "https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c"
-  // remember to change this to your own endpoint!
+  "emerge way beef hawk elbow foil satoshi magnet like car glue survey",
+  "https://rinkeby.infura.io/v3/a79c40a8fb7e437d89f93e722456ef6a"
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", accounts[0]);
+  console.log("attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ gas: "1000000", from: accounts[0] });
-
-  console.log("Contract deployed to", result.options.address);
+    .deploy({
+      data: bytecode,
+    })
+    .send({ gas: "1000000", gasPrice: "5000000000", from: accounts[0] });
+  console.log("Contract deployed to :", result.options.address);
 };
 deploy();
